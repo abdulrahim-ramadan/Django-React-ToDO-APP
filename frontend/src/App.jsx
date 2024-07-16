1aa8d2534ace40a8bcf7d3ed223637aef1a1084c
abdulrahim-ramadan <abdalrhim19991@gmail.com>
2024-07-16 10:32:19 +0200

    design UI using bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react'
+
+import ToDos from './components/Todo';  
+import CreateToDo from './components/CreatTodo';
+
+
 
 
 function App() {
-  
+
+  const [todos, setTodos] = useState([]);
+  //get todos from API
+  useEffect(()=>{
+    fetch("http://127.0.0.1:8000/todo/")
+    .then(response => response.json())
+    .then(data => setTodos(data))
+  })
 
   return (
-    <>
+    <div className='container '>
+      <div className='row'>
+        <div className='col-lg-6 mx-auto my-5'>
+        <CreateToDo />
+        <ToDos todos={todos} setTodos={setTodos} />
+        </div>
+      </div>
 
-    </>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Todo.jsx
@@ -0,0 +1,26 @@
+
+import { useState } from "react";
+
+const ToDos = ({todos,setTodos}) =>{
+
+
+    return (
+        <div className="container mt-4">
+            <ul className="list-group">
+                {todos.map((todo, index) => (
+                    <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                        <span>{todo.title}</span>
+                            <button
+                                className={'btn btn-sm border'}
+                            >
+                                {todo.status}
+                           </button>
+                        
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ToDos
