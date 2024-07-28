7afa32a0e4e15e07015c792c6cc4604b59e74b44
abdulrahim-ramadan <abdalrhim19991@gmail.com>
2024-07-28 04:30:00 +0200

    add update todo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,12 +17,17 @@ function App() {
     .then(data => setTodos(data))
   })
 
+  // update todo
+  const updateTodoEvent = (updatedTodo) =>{
+    setTodos(todos.map(todo => (todo.id == updatedTodo.id ? updatedTodo : todo)))
+  }
+
   return (
     <div className='container '>
       <div className='row'>
         <div className='col-lg-6 mx-auto my-5'>
         <CreateToDo />
-        <ToDos todos={todos} setTodos={setTodos} />
+        <ToDos todos={todos} setTodos={setTodos} onUpdateTodo={updateTodoEvent}/>
         </div>
       </div>
 
--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,8 +1,27 @@
-
 import { useState } from "react";
 
-const ToDos = ({todos,setTodos}) =>{
+const ToDos = ({ todos, setTodos,onUpdateTodo }) => {
 
+    // update todo
+    const handleToDoUpdate = (todo) => {
+
+        const updatedStatus = todo.status == "DONE" ? "IN PROGRESS" : "DONE";
+
+        fetch(`http://127.0.0.1:8000/todo/${todo_id}/`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ status: updatedStatus , title:todo.title })
+        })
+        .then(response => { 
+            return response.json()
+            // Hier könntest du die todos aktualisieren, basierend auf der Antwort
+        })
+        .then(data => {
+          onUpdateTodo(data) // todo update
+        })
+    }
 
     return (
         <div className="container mt-4">
@@ -10,17 +29,17 @@ const ToDos = ({todos,setTodos}) =>{
                 {todos.map((todo, index) => (
                     <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
                         <span>{todo.title}</span>
-                            <button
-                                className={'btn btn-sm border'}
-                            >
-                                {todo.status}
-                           </button>
-                        
+                        <button
+                            className={'btn btn-sm border'}
+                            onClick={() => handleToDoUpdate(todo)}
+                        >
+                            {todo.status}
+                        </button>
                     </li>
                 ))}
             </ul>
         </div>
-    )
+    );
 }
 
-export default ToDos
+export default ToDos;
